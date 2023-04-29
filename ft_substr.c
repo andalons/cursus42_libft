7b7d3c89e8402342080447e831db4ba05556c264
Andrea Alonso <andreacor001@gmail.com>
2023-04-29 11:45:36 +0200

    minor changes in substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: aalonso <aalonso@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 09:29:17 by aalonso           #+#    #+#             */
-/*   Updated: 2023/04/28 15:39:06 by aalonso          ###   ########.fr       */
+/*   Updated: 2023/04/29 11:44:29 by aalonso          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,22 @@
  * @param str starting string
  * @param start index from which substring will start
  * @param len max length of the substring
-
- * 		ft_strlen() is used to return an empty string when start is beyond end.
- * 		The length of the substring is calculated by taking the minimum of len
- * 		and the difference between the length of str and start.
+ * 
+ * 		ft_strdup() is used to return an empty string when start is beyond end.
  */
 char	*ft_substr(char const *str, unsigned int start, size_t len)
 {
 	size_t	str_len;
-	size_t	substr_len;
 	char	*substr;
 
 	str_len = ft_strlen(str);
-	substr_len = len;
 	if (start >= str_len)
 		return (ft_strdup(""));
 	if ((str_len - start) < len)
-		substr_len = str_len - start;
-	substr = malloc((substr_len + 1) * sizeof(char));
+		len = str_len - start;
+	substr = malloc((len + 1) * sizeof(char));
 	if (substr == NULL)
 		return (NULL);
-	ft_strlcpy(substr, str + start, substr_len + 1);
+	ft_strlcpy(substr, str + start, len + 1);
 	return (substr);
 }
